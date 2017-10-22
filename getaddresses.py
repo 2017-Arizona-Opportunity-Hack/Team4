75ce3a80639ba972bc8b876c92b9244443408c54
Jonathan Gill <jtgill@email.arizona.edu>
2017-10-21 21:48:48 -0700

    Integrated usage of geocode
    Uses geocode library to convert addresses to latlong

--- getaddresses.py
+++ getaddresses.py
@@ -1,7 +1,10 @@
 import pandas as pd
+import geocoder
+import csv
 
 data_missing = pd.read_csv('Hackathon_Missing_Child_5_Years_of_Data.csv')
-data_attempt = pd.read_csv('Attempts_Hackathon_5_Years_of_Data.csv')
+#data_attempt = pd.read_csv('Attempts_Hackathon_5_Years_of_Data.csv')
+data_attempt = pd.read_csv('Attempts_Hackathon_5_Years_of_Data2.csv')
 
 #data_missing = data_missing[data_missing.name.notnull()]
 #data_attempt = data_attempt[data_attempt.name.notnull()]
@@ -20,7 +23,7 @@ def get_addresses():
 		location = list(attempt_locations_raw.iloc[i])
 
 		try:
-			# Zip codes are pulled down as floats
+			# Zip codes are pulled down as floats.
 			location[-1] = str(int(location[-1]))
 		except:
 			#print('Ran across a NaN zip code. No worries, I\'ll ignore it.')
@@ -32,7 +35,7 @@ def get_addresses():
 			print('{0}% finished'.format(i/len(data_attempt)*100))
 
 		# Appends tuple of (case_number, address) to the list of locations if
-		# the incident location isn't 'unknown'
+		# the incident location isn't 'unknown'.
 		if 'unknown' not in location[1].lower():
 			locations.append((int(location[0]), ' '.join(location[1:])))
 
@@ -42,6 +45,21 @@ def get_addresses():
 
 if __name__ == '__main__':
 	locs = get_addresses()
-	print(locs[0])
-	print(locs[100])
-	print(locs[205])
+	#print(locs[0])
+	#print(locs[100])
+	#print(locs[205])
+
+	#for i in range(len(locs)):
+	with open('Attempts_Hackathon_5_YearsOutput.csv', 'w') as file1:
+		fieldnames = ['Incident City', 'GEO', 'Case Number']
+		writer = csv.DictWriter(file1, fieldnames = fieldnames)
+		for i in range(len(locs)):
+			g = geocoder.google(locs[i][1])
+			print(locs[i][1], locs[i][0], g.latlng)
+			writer.writerow({'Incident City': str(g.latlng), 'Case Number' : str(locs[i][0])})
+			print(locs[i][1])
+		
+		if (i*100/(int(len(locs)/10)*10) % 5 == 0):
+			print('\n\t{0}% finished\n'.format(i/len(data_attempt)*100))
+
+	
